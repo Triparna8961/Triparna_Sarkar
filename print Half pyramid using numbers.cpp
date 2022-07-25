0b53cc63b8b4d93893529eb396907f8e6028c1ed
Triparna8961 <109430584+Triparna8961@users.noreply.github.com>
2022-07-25 11:03:51 +0530

    Create print Half pyramid using numbers.cpp

---
+++ print Half pyramid using numbers.cpp
@@ -0,0 +1,18 @@
+include<iostream>
+using namespace std;
+int main()
+{
+  int n,m;
+  cout<<"Enter the  number of n:-"<<endl;
+  cin>>n;
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=1;j<=i;j++)
+    {
+      m=1;
+      cout<<m<<" ";
+    }
+    cout<<endl;
+  }
+  return 0;
+}
